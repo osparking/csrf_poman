11852c0b36e020e73fc4d284df7bb30d78cf53cf
osparking <jbpark03@gmail.com>
2023-08-07 15:53:51 +0900

    started with 2 dependencies

---
+++ src/main/java/space/bumsoap/sec/csrf_poman/CsrfPomanApplication.java
@@ -0,0 +1,13 @@
+package space.bumsoap.sec.csrf_poman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfPomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrfPomanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumsoap/sec/csrf_poman/CsrfPomanApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumsoap.sec.csrf_poman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrfPomanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
